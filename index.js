dce2a6bf6e152088604819a4742916f5ac56d44a
dani <dani.zaghian@gmail.com>
2020-10-07 11:52:41 -0700

    adds webpack configurations and babel

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-var moment = require("moment");
-var math = require("mathjs");
+import moment from "moment";
+import { sqrt } from "mathjs";
 
-var message = "Hello from JavaScript!";
+let message = "Hello from JavaScript!";
 console.log(message);
 
-var datetime = moment().startOf("day").fromNow();
+let datetime = moment().startOf("day").fromNow();
 console.log(datetime);
 
-console.log(math.sqrt(-4));
+console.log(sqrt(-4));
+
+let name = "Bob";
+let time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
