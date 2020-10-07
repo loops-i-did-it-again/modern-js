8aa9e4ee6c2993437e34767e451776cba685cd4a
dani <dani.zaghian@gmail.com>
2020-10-07 10:56:58 -0700

    adds webpack module bundler and mathjs dependency

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+var moment = require("moment");
+var math = require("mathjs");
+
 var message = "Hello from JavaScript!";
 console.log(message);
 
 var datetime = moment().startOf("day").fromNow();
 console.log(datetime);
+
+console.log(math.sqrt(-4));
